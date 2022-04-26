030d1e765cdf4e6fcf6201b9173d520bfb31c219
SanjaVasiljevic <sanja.vasiljevic@hotmail.com>
2022-04-26 12:18:21 +0200

    Alas project

--- Automation Task 1/SauceDemo/src/test/java/PagesSauceDemo/ScreenshotBase.java
+++ Automation Task 1/SauceDemo/src/test/java/PagesSauceDemo/ScreenshotBase.java
@@ -1,17 +1,14 @@
 package PagesSauceDemo;
-
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-
 import java.io.File;
 import java.io.IOException;
 
 public class ScreenshotBase {
 
     public WebDriver driver;
-
     public void takeScreenshot() throws IOException {
         File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
         String currentDir = "D:\\Alas D.O.O intervju\\AlasDOO_Project";
